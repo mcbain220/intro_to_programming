3308e9c5ac9884d79431890e8a86affefa068dca
Chris Keaney <chris.keaney@gmail.com>
2014-07-16 08:03:04 -0400

    arrays

---
+++ arrays/arrays_exercise_1.rb
@@ -0,0 +1,7 @@
+# arrays excercise 1
+
+arr = [1,3,4,5,9,11]
+
+number = 3
+
+puts "The array contains the number #{number}." if arr.include?(number)
---
+++ arrays/arrays_exercise_7.rb
@@ -0,0 +1,15 @@
+# arrays exercise 7
+
+original_array = [1,2,3,4,5]
+new_array = []
+
+original_array.each do |i|
+  new_array << (i + 2)
+end
+
+  
+
+p original_array
+p new_array
+ 
+
